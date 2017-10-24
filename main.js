99716cffe7947d39121c8e72823b2b41eeb39dce
Maya Noelle <maya.n.brooks@gmail.com>
2017-10-24 17:36:29 -0400

    10.24 commit

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
-// Initialize collapse button
-$(".button-collapse").sideNav();
-// Initialize collapsible (uncomment the line below if you use the dropdown variation)
-//$('.collapsible').collapsible();
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+});
 
-
-$('.button-collapse').sideNav({
-     menuWidth: 300, // Default is 300
-     edge: 'right', // Choose the horizontal origin
-     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-     draggable: true, // Choose whether you can drag to open on touch screens,
-     onOpen: function(el) { }
-   }
- );
-
-onclick.$('.button-collapse').sideNav('show');
+$(document).ready(function() {
+ $(".dropdown-button").dropdown({hover: false});
+});
